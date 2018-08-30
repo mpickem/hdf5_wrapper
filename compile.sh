e48e3e2106c2bb719f7ad55fc0009ff3dc8b0dd0
Matthias Pickem <matthias.pickem@gmail.com>
2018-08-30 14:50:55 +0200

    Bug fix for general compilation.
    One needs to export the LD_LIBRARY_PATH variable, because hdf5 uses dynamics libraries.

--- compile.sh
+++ compile.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-gfortran -c hdf5_wrapper.f90 -o hdf5_wrapper.o -I/opt/hdf5-1.8.16_gcc/include -L/opt/hdf5-1.8.16_gcc/lib -lhdf5_fortran -lhdf5hl_fortran
-gfortran -c test_examples.f90 -o test_examples.o -I/opt/hdf5-1.8.16_gcc/include -L/opt/hdf5-1.8.16_gcc/lib -lhdf5_fortran -lhdf5hl_fortran
-gfortran test_examples.o hdf5_wrapper.o -o main -I/opt/hdf5-1.8.16_gcc/include -L/opt/hdf5-1.8.16_gcc/lib -lhdf5_fortran -lhdf5hl_fortran -fbounds-check -Wall -fbacktrace
+gfortran -c hdf5_wrapper.f90 -o hdf5_wrapper.o -I/opt/hdf5-1.8.20_gcc/include
+gfortran -c test_examples.f90 -o test_examples.o -I/opt/hdf5-1.8.20_gcc/include
+gfortran test_examples.o hdf5_wrapper.o -o main -I/opt/hdf5-1.8.20_gcc/include -L/opt/hdf5-1.8.20_gcc/lib -lhdf5_fortran -lhdf5hl_fortran
 
+
+export LD_LIBRARY_PATH=/opt/hdf5-1.8.20_gcc/lib:$LD_LIBRARY_PATH
 ./main
 
